f6d5556bc6801abb0bb21d4ca3cd24707fab4ea2
surasithaof <surasithpc@gmail.com>
2024-01-02 11:38:55 +0700

    chore: update dependencies

--- PDF.Generator.WebAPI/Services/Interfaces/IPdfGeneratorService.cs
+++ PDF.Generator.WebAPI/Services/Interfaces/IPdfGeneratorService.cs
@@ -2,9 +2,8 @@ namespace PDF.Generator.WebAPI.Services.Interfaces;
 
 public interface IPdfGeneratorService
 {
-    Task<byte[]> GeneratePdf(string templateFullPath, object templateData, string headerText = null,
-        bool hasPageNumber = true);
-    
+    Task<byte[]> GeneratePdf(string templateFullPath, object templateData, string? headerText = null, bool hasPageNumber = true);
+
     byte[] ConcatenatePdfs(IEnumerable<byte[]> documents);
 
     byte[] StampMergedPdfNumber(byte[] document);
